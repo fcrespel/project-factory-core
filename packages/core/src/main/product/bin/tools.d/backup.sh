e59362b0f05f2fb01418cd5972e646ee55ca109f
Fabien Crespel <fabien@crespel.net>
2016-04-11 23:38:58 +0200

    Core: add backup/restore commands (refs #4120)

---
+++ packages/core/src/main/product/bin/tools.d/backup.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#
+# Backup script.
+# By Fabien CRESPEL <fabien@crespel.net>
+#
+
+# Shared routines
+SCRIPT_DIR=`dirname "$0"`
+. "$SCRIPT_DIR/../loadenv.sh"
+
+# Script variables
+BACKUP_DIR="$PRODUCT_BIN/backup.d"
+SERVICE_NAME="${1%.sh}"
+BACKUP_SCRIPT="$BACKUP_DIR/backup-$SERVICE_NAME.sh"
+
+# Check user
+if [ "$(whoami)" != "root" ]; then
+	echo "You must be root to run this command"
+	exit 1
+fi
+
+# Check arguments
+if [ -z "$SERVICE_NAME" ]; then
+	echo "Usage: $0 <service name>"
+	echo "Available services: "
+	find "$BACKUP_DIR" -name 'backup-*.sh' -printf '%f\n' | sort | while read SERVICE; do
+		SERVICE="${SERVICE#backup-}"
+		echo "  ${SERVICE%.sh}"
+	done
+	exit 1
+elif [ ! -e "$BACKUP_SCRIPT" ]; then
+	echo "Service $SERVICE_NAME does not exist or does not support backup."
+	exit 2
+fi
+
+# Execute backup
+echo "Executing $BACKUP_SCRIPT ..."
+chmod +x "$BACKUP_SCRIPT"
+if "$BACKUP_SCRIPT"; then
+	echo "$BACKUP_SCRIPT completed successfully"
+else
+	RET=$?
+	echo "ERROR: $BACKUP_SCRIPT failed with exit code: $RET"
+	exit $RET
+fi
---
+++ packages/core/src/main/product/bin/tools.d/restore.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#
+# Restore script.
+# By Fabien CRESPEL <fabien@crespel.net>
+#
+
+# Shared routines
+SCRIPT_DIR=`dirname "$0"`
+. "$SCRIPT_DIR/../loadenv.sh"
+
+# Script variables
+BACKUP_DIR="$PRODUCT_BIN/backup.d"
+SERVICE_NAME="${1%.sh}"
+RESTORE_SCRIPT="$BACKUP_DIR/restore-$SERVICE_NAME.sh"
+
+# Check user
+if [ "$(whoami)" != "root" ]; then
+	echo "You must be root to run this command"
+	exit 1
+fi
+
+# Check arguments
+if [ -z "$SERVICE_NAME" ]; then
+	echo "Usage: $0 <service name>"
+	echo "Available services: "
+	find "$BACKUP_DIR" -name 'restore-*.sh' -printf '%f\n' | sort | while read SERVICE; do
+		SERVICE="${SERVICE#restore-}"
+		echo "  ${SERVICE%.sh}"
+	done
+	exit 1
+elif [ ! -e "$RESTORE_SCRIPT" ]; then
+	echo "Service $SERVICE_NAME does not exist or does not support restore."
+	exit 2
+fi
+
+# Execute restore
+echo "Executing $RESTORE_SCRIPT ..."
+chmod +x "$RESTORE_SCRIPT"
+if "$RESTORE_SCRIPT"; then
+	echo "$RESTORE_SCRIPT completed successfully"
+else
+	RET=$?
+	echo "ERROR: $RESTORE_SCRIPT failed with exit code: $RET"
+	exit $RET
+fi
